1233efa8c32073dbbfd360ec8d38a3c9c0572e8f
parsaee <parsaee@W7-IN-21.Hedcoint.com>
2019-04-07 16:01:43 +0430

    apply jss to app

--- src/app.js
+++ src/app.js
@@ -7,19 +7,27 @@ import { startSetProvinces } from './actions/provinces';
 import { startSetProducts } from './actions/products';
 import { startSetCustomers } from './actions/customers';
 import { startSetInvoices } from './actions/invoices';
+import { create } from 'jss';
+import { StylesProvider, jssPreset } from '@material-ui/styles';
+import rtl from 'jss-rtl'
 
 // import Header from './components/Header';
 import './styles/styles.scss';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 
+const jss = create({
+  plugins: [...jssPreset().plugins, rtl()],
+});
 
 const store = configureStore();
 
 const jsx = (
-  <Provider store={store}>
-    <AppRouter />
-  </Provider>
+  <StylesProvider jss={jss}>
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+  </StylesProvider>  
 );
 
 ReactDOM.render(<p>Loading ...</p>, document.getElementById('app'));
