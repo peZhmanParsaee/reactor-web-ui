1f30bd054f5c7b00b60f3960a8a1454c5c1ba299
peZhmanParsaee <p.parsaee@gmail.com>
2019-04-10 11:24:05 +0430

    clean codes in app.js

--- src/app.js
+++ src/app.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { create } from 'jss';
+import { StylesProvider, jssPreset, createGenerateClassName } from '@material-ui/styles';
+import rtl from 'jss-rtl'
+
 import configureStore from './store/configureStore';
 import AppRouter from './router/AppRouter';
 import { startSetProvinces } from './actions/provinces';
 import { startSetProducts } from './actions/products';
-import { startSetCustomers } from './actions/customers';
-import { startSetInvoices } from './actions/invoices';
-import { create } from 'jss';
 import Loading from './components/Loading';
-import { StylesProvider, jssPreset, createGenerateClassName } from '@material-ui/styles';
-import rtl from 'jss-rtl'
 
-// import Header from './components/Header';
 import './styles/styles.scss';
-import 'react-dates/initialize';
-import 'react-dates/lib/css/_datepicker.css';
 
 const jss = create({
   plugins: [...jssPreset().plugins, rtl()],
@@ -38,9 +34,7 @@ ReactDOM.render(<Loading />, document.getElementById('app'));
 
 store.dispatch(startSetProvinces()).then(() => {
   store.dispatch(startSetProducts()).then(() => {
-    // store.dispatch(startSetCustomers()).then(() => {
-      ReactDOM.render(jsx, document.getElementById('app'));
-    // });
+    ReactDOM.render(jsx, document.getElementById('app'));
   });
 });
 
@@ -51,5 +45,4 @@ store.subscribe(() => {
   } else {
     ReactDOM.render(jsx, document.getElementById('app'));
   }
-  console.log(state);
 });
