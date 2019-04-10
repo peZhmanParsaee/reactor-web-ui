4743b48b381bb0bcc940fb4e0223fccbd003bced
peZhmanParsaee <p.parsaee@gmail.com>
2019-04-10 11:23:45 +0430

    clean codes in SalesReportPage Component

--- src/components/SalesReportPage.js
+++ src/components/SalesReportPage.js
@@ -1,95 +1,41 @@
 import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { withStyles } from '@material-ui/core/styles';
 import axios from 'axios';
 import moment from 'moment-jalaali';
-import { generateKey } from '../helpers/keyHelper';
-
-
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import JalaliUtils from "@date-io/jalaali";
+import { DatePicker, MuiPickersUtilsProvider } from "material-ui-pickers";
 
-
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-
-
+// @material-ui/core
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import TypoGraphy from '@material-ui/core/Typography';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
-
-// components
-import Header from './Header';
-import ToastMessage from './ToastMessage';
-import GridContainer from './Grid/GridContainer';
-import GridItem from './Grid/GridItem';
-import Footer from "./Footer";
-import { separateDigits } from '../helpers/numberHelpers';
-
-// theme
-import appTheme from '../themes/AppTheme';
-import { finished } from 'stream';
-
-
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import CommentIcon from '@material-ui/icons/Comment';
-import Paper from '@material-ui/core/Paper';
-
-
-import PropTypes from 'prop-types';
-
-
 import Drawer from '@material-ui/core/Drawer';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import Divider from '@material-ui/core/Divider';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 
-import {
-  DatePicker,
-  TimePicker,
-  DateTimePicker,
-  MuiPickersUtilsProvider
-} from "material-ui-pickers";
-import JalaliUtils from "@date-io/jalaali";
+// local components
+import Footer from "./Footer";
 
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
-
+// local dependencies
+import { separateDigits } from '../helpers/numberHelpers';
+import { generateKey } from '../helpers/keyHelper';
 import appStyle from '../styles/jss/layouts/appStyle';
-
-
-import { SingleDatePicker } from 'react-dates';
-import Calendar from 'material-ui-pickers/DatePicker/components/Calendar';
-
-
-const renderMonthText = (input) => {  
-  return input.format('jMM');
-  return null;
-}
-
-const renderDayContents = (input) => {
-  return input.format('DD');
-  return null;
-}
 
 
 class SalesReportPage extends React.PureComponent {
@@ -250,7 +196,7 @@ class SalesReportPage extends React.PureComponent {
     this.setState({ selectedDate: date })
   };
   render() {
-    const { classes, theme } = this.props;
+    const { classes } = this.props;
 
     moment.locale('fa-IR');
     moment.loadPersian();
@@ -311,43 +257,6 @@ class SalesReportPage extends React.PureComponent {
                     }}
                   />
                 </ListItem>
-
-                {
-                  // <ListItem key={2}>
-                  //   <div style={{position: 'absolute'}}>
-                  //     <SingleDatePicker
-                  //       date={this.state.fromDate}
-                  //       onDateChange={this.onFromDateChange}
-                  //       focused={this.state.fromDateCalendarFocused}
-                  //       onFocusChange={this.onFromDateFocusChange}
-                  //       numberOfMonths={1}
-                  //       anchorDirection="right"
-                  //       showDefaultInputIcon
-                  //       isRTL
-                  //       monthFormat="MMMM YYYY"
-                  //       placeholder="از تاریخ"
-                  //       renderMonthText={renderMonthText} 
-                  //       renderDayContents={renderDayContents}
-                  //     />
-                  //   </div>                  
-                  // </ListItem>
-                  // <ListItem key={3}>
-                  //   <SingleDatePicker
-                  //     date={this.state.toDate}
-                  //     onDateChange={this.onToDateChange}
-                  //     focused={this.state.toDateCalendarFocused}
-                  //     onFocusChange={this.onToDateFocusChange}
-                  //     numberOfMonths={1}
-                  //     isOutsideRange={() => false}
-                  //     numberOfMonths={1}
-                  //     anchorDirection="right"
-                  //     showDefaultInputIcon
-                  //     isRTL
-                  //     monthFormat="YYYY/MM/DD"
-                  //     placeholder="تا تاریخ"
-                  //   />
-                  // </ListItem>
-                }
                 
                 <ListItem key={5}>
                   <MuiPickersUtilsProvider utils={JalaliUtils} locale="fa">
@@ -394,7 +303,6 @@ class SalesReportPage extends React.PureComponent {
                 </ListItem>
                 <ListItem key={12}>
                   <FormControl component="fieldset" className={classes.formControl}>
-                    {/* <FormLabel component="legend">نوع فاکتور</FormLabel> */}
                     <RadioGroup
                       aria-label="gender"
                       name="invoiceType"
@@ -494,22 +402,6 @@ class SalesReportPage extends React.PureComponent {
                     {this.state.loadingState ? <p className="loading"> در حال بارگزاری ادامه داده ها ...</p> : ""}
             
                   </div>
-                  
-                  {
-
-                    // <GridContainer className={classes.grid} justify="space-around">
-                    //   <MuiPickersUtilsProvider>
-                    //     <DatePicker
-                    //       margin="normal"
-                    //       label="Date picker"
-                    //       value={this.state.selectedDate}
-                    //       onChange={this.handleDateChange}
-                    //     />
-                    //   </MuiPickersUtilsProvider>                    
-                    // </GridContainer>
-                  }
-                  
-
                 </div>              
               </div>
             
