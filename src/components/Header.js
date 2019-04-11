f799a0ebf357e55b18ee687c0b5fed0b9c0659c9
pezhmanparsaee <p.parsaee@gmail.com>
2019-04-11 18:11:46 +0430

    apply header jss to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+// @material-ui/core
+import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import TypoGraphy from '@material-ui/core/Typography';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
 
-const Header = () => {
+import headerStyle from '../styles/jss/components/headerStyle';
+
+const Header = (props) => {
+  const { classes } = props;
+
   return (
-    <div>
+    <header>
       <AppBar position="static">
         <Toolbar>
           <TypoGraphy variant="h6" color="inherit" noWrap>
-            <ShoppingCartIcon className="header__app-icon" />
+            <ShoppingCartIcon className={classes.headerAppIcon} />
             ری‌اکتور
           </TypoGraphy>
         </Toolbar>
       </AppBar>
-    </div>
+    </header>
   );
 };
 
-export default Header;
+Header.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(headerStyle)(Header);
