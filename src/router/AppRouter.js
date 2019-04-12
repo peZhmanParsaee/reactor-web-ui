1ff639b6ca4df6bd7cfcdcffdabbb57e7fd43f5c
pezhmanparsaee <p.parsaee@gmail.com>
2019-04-12 17:26:57 +0430

    remove old routes from AppRouter

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,29 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from '../components/HomePage';
+import { Router, Route, Switch } from 'react-router-dom';
 import NotFoundPage from '../components/NotFoundPage';
-// import Header from '../components/Header';
-import AddInvoicePage from '../components/AddInvoicePage';
-import SalesReportPage from '../components/SalesReportPage';
-// import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import AppTheme from '../themes/AppTheme';
-import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 import { createBrowserHistory } from "history";
 
-const history = createBrowserHistory()
+import Layout from '../layouts/Layout';
 
-const theme = createMuiTheme({
-  direction: 'rtl',
-});
+const history = createBrowserHistory()
 
 const AppRouter = () => (
   <MuiThemeProvider theme={AppTheme}>
     <Router history={history}>
       <div>
         <Switch>
-          <Route path="/" component={HomePage} exact={true} />
-          <Route path="/add-invoice" component={AddInvoicePage} />
-          <Route path="/sales-report" component={SalesReportPage} />
+          <Route path="/" component={Layout} />
           <Route component={NotFoundPage} />
         </Switch>
       </div>
