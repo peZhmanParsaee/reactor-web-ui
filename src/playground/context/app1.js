b9771030e0b3d18ac62e17e10851500d0e293972
peZhmanParsaee <p.parsaee@gmail.com>
2019-04-13 12:13:48 +0430

    add a context sample to playground

---
+++ src/playground/context/app1.js
@@ -0,0 +1,48 @@
+import {ThemeContext, themes} from './theme-context';
+import ThemedButton from './themed-button';
+import React from 'react';
+
+// An intermediate component that uses the ThemedButton
+function Toolbar(props) {
+  return (
+    <ThemedButton onClick={props.changeTheme}>
+      Change Theme
+    </ThemedButton>
+  );
+}
+
+class App1 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: themes.light,
+    };
+
+    this.toggleTheme = () => {
+      this.setState(state => ({
+        theme:
+          state.theme === themes.dark
+            ? themes.light
+            : themes.dark,
+      }));
+    };
+  }
+
+  render() {
+    // The ThemedButton button inside the ThemeProvider
+    // uses the theme from state while the one outside uses
+    // the default dark theme
+    return (
+      <React.Fragment>
+        <ThemeContext.Provider value={this.state.theme}>
+          <Toolbar changeTheme={this.toggleTheme} />
+        </ThemeContext.Provider>
+        <div>
+          <ThemedButton />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App1;
---
+++ src/playground/context/themed-button.js
@@ -0,0 +1,18 @@
+import {ThemeContext} from './theme-context';
+import React from 'react';
+
+class ThemedButton extends React.Component {
+  render() {
+    let props = this.props;
+    let theme = this.context;
+    return (
+      <button
+        {...props}
+        style={{backgroundColor: theme.background}}
+      />
+    );
+  }
+}
+ThemedButton.contextType = ThemeContext;
+
+export default ThemedButton;
