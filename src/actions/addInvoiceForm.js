2291a5079ba39d605b6be8c52619f3fc2c198eef
peZhmanParsaee <p.parsaee@gmail.com>
2019-04-14 17:26:59 +0430

    add rest of action generators for addInvoiceForm

--- src/actions/addInvoiceForm.js
+++ src/actions/addInvoiceForm.js
@@ -1,9 +1,22 @@
 import * as TYPES from './types';
 
+export const invoiceFormClearState = () => {
+    return {
+        type: TYPES.INVOICE_FORM_CLEAR_STATE
+    };
+};
+
 export const invoiceFormSetAddProductDialogOpenState = (isAddProductDialogOpen) => {
     return {
       type: TYPES.INVOICE_FORM_SET_ADD_PRODUCT_DIALOG_OPEN,
       payload: { isAddProductDialogOpen }
+    };
+};
+
+export const invoiceFormSetInvoiceNo = (invoiceNo) => {
+    return {
+      type: TYPES.INVOICE_FORM_SET_INVOICE_NO,
+      payload: { invoiceNo }
     };
 };
 
@@ -14,16 +27,90 @@ export const invoiceFormSetCustomerId = (customerId) => {
     };
 };
 
-export const invoiceFormSetSuggestions = (suggestions) => {
+export const invoiceFormSetSuggestions = ({ single, suggestions }) => {
     return {
         type: TYPES.INVOICE_FORM_SET_SUGGESTIONS,
-        payload: { suggestions }
+        payload: { single, suggestions }
     };
 };
 
-export const invoiceFormSetSuggestion = (suggestion) => {
+export const invoiceFormSetSuggestion = ({ name, customerId, single }) => {
     return {
         type: TYPES.INVOICE_FORM_SET_SUGGESTION,
-        payload: { suggestion }
+        payload: { name, customerId, single }
+    };
+};
+
+export const invoiceFormAddNewProductToInvoice = (newInvoiceProduct) => {
+    return {
+        type: TYPES.INVOICE_FORM_ADD_NEW_PRODUCT_TO_INVOICE,
+        payload: { newInvoiceProduct }
+    };
+};
+
+export const invoiceFormSetNewProduct = ({ newProduct, newInvoiceProduct }) => {
+    return {
+        type: TYPES.INVOICE_FORM_SET_NEW_PRODUCT,
+        payload: { newProduct, newInvoiceProduct }
+    };
+};
+
+export const invoiceFormRemoveProductFromInvoiceById = (invoiceProductId) => {
+    return {
+        type: TYPES.INVOICE_FORM_REMOVE_PRODUCT_FROM_INVOICE_BY_ID,
+        payload: { invoiceProductId }
+    };
+};
+
+export const invoiceFormSetInvoiceProductCount = ({ invoiceProductId, count }) => {
+    return {
+        type: TYPES.INVOICE_FORM_SET_INVOICE_PRODUCT_COUNT,
+        payload: {
+            invoiceProductId, 
+            count
+        }
+    };
+};
+
+export const invoiceFormSetProvinceId = (provinceId) => {
+    return {
+        type: TYPES.INVOICE_FORM_SET_PROVINCE_ID,
+        payload: { provinceId }
+    };
+};
+
+
+export const invoiceFormSetCityId = (cityId) => {
+    return {
+        type: TYPES.INVOICE_FORM_SET_CITY_ID,
+        payload: { cityId }
+    };
+};
+
+export const invoiceFormSetMailType = (mailType) => {
+    return {
+        type: TYPES.INVOICE_FORM_SET_MAIL_TYPE,
+        payload: { mailType }
+    };
+};
+
+export const invoiceFormSetDeliverAfter = (deliverAfter) => {
+    return {
+        type: TYPES.INVOICE_FORM_SET_DELIVER_AFTER,
+        payload: { deliverAfter }
+    };
+};
+
+export const invoiceFormSetDeliverAfterTimeUnit = (deliverAfterTimeUnit) => {
+    return {
+        type: TYPES.INVOICE_FORM_SET_DELIVER_AFTER_TIME_UNIT,
+        payload: { deliverAfterTimeUnit }
+    };
+};
+
+export const invoiceFormSetActiveStep = (activeStep) => {
+    return {
+        type: TYPES.INVOICE_FORM_SET_ACTIVE_STEP,
+        payload: { activeStep }
     };
 };
