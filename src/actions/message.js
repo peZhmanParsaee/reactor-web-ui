bfda59c69baf373c3dbdd68b0ec2edcc00359264
peZhmanParsaee <p.parsaee@gmail.com>
2019-04-14 17:23:21 +0430

    modify message action generator

--- src/actions/message.js
+++ src/actions/message.js
@@ -1,8 +1,11 @@
 import * as TYPES from './types';
 
-export const showGlobalMessage = (message) => {
+export const showGlobalMessage = ({ type, text }) => {
   return {
-    type: TYPES.SHOW_LOADING,
-    message
+    type: TYPES.SHOW_MESSAGE,
+    payload:{
+      type,
+      text
+    }
   };
 };
