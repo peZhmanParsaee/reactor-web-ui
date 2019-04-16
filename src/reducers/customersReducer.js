603d9f75d0c88d184c303956977c8cce929c5195
peZhmanParsaee <p.parsaee@gmail.com>
2019-04-16 16:17:42 +0430

    fix bug in customersReducer.js

--- src/reducers/customersReducer.js
+++ src/reducers/customersReducer.js
@@ -3,11 +3,10 @@ import * as TYPES from '../actions/types';
 const INITIAL_STATE = [];
 
 export default (state = INITIAL_STATE, action) => {  
+  
   switch(action.type) {
     case TYPES.SET_CUSTOMERS:
-      return action.customers;
-    case TYPES.SEARCH_CUSTOMERS:
-      return action.customers;
+      return [...action.customers];
     default:
       return state;
   }
