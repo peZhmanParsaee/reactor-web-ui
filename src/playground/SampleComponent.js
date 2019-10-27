cd4ef549c0b898df4c3f050bb4a8700f0174d735
pezhmanparsaee <p.parsaee@gmail.com>
2019-10-27 15:23:15 +0330

    Add new SampleComponent.js

---
+++ src/playground/SampleComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class SampleComponent extends Component {
+  render() {
+    return (
+      <div>It is from SampleComponent</div>
+    );
+  }
+}
