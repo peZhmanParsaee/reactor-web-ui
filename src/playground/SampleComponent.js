674e9609cc96b1d87815dd286790f35560fc372d
pezhmanparsaee <p.parsaee@gmail.com>
2019-10-27 15:36:42 +0330

    Add componentDidMount for SampleComponent

--- src/playground/SampleComponent.js
+++ src/playground/SampleComponent.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 export default class SampleComponent extends Component {
+  componentDidMount() {
+    console.log('hi');
+  }
   render() {
     return (
       <div>It is from SampleComponent</div>
