da233b74b7b353591b3ac625e206be5dce3d3950
pezhmanparsaee <p.parsaee@gmail.com>
2020-05-10 18:03:08 +0430

    fix some linting problems in api.js

--- src/api/api.js
+++ src/api/api.js
@@ -5,89 +5,86 @@ import axios from 'axios';
 const getUrl = (route) => `${API_ENDPOINT}/api/${route}`;
 
 const getRequestHeaders = (withAuth) => ({
-    "Authorization": withAuth ? `Bearer ${localStorage['token']}` : undefined,
-    "Content-Type": "application/json; charset=utf-8",        
-    'Access-Control-Allow-Origin' : '*'
+  "Authorization": withAuth ? `Bearer ${localStorage['token']}` : undefined,
+  "Content-Type": "application/json; charset=utf-8",        
+  'Access-Control-Allow-Origin' : '*'
 });
 
 const apiCall = ({
-    url,
-    method = 'GET',
-    data = null,
-    withAuth = false,
-    params = null
+  url,
+  method = 'GET',
+  data = null,
+  withAuth = false,
+  params = null
 }) => {
-    console.log('apiCall')
-    console.log(url, method)
-
-    return axios
-        .request({
-            url: getUrl(url),
-            method,
-            // headers: getRequestHeaders(withAuth),
-            data,
-            params
-        })
-        .catch(error => {            
-            if (error.response && error.response.status === 403) {
-                // dispatch(accessDenied(window.location.pathname));
-            } else {
-                throw error;
-            }
-        });
+  return axios
+    .request({
+      url: getUrl(url),
+      method,
+      // headers: getRequestHeaders(withAuth),
+      data,
+      params
+    })
+    .catch(error => {            
+      if (error.response && error.response.status === 403) {
+        // dispatch(accessDenied(window.location.pathname));
+      } else {
+        throw error;
+      }
+    });
 }
 
 export const getCustomers = () => {
-    return apiCall({
-        url: 'v1/customer'
-    });
+  return apiCall({
+    url: 'v1/customer'
+  });
 }
 
 export const searchCustomers = customerName => {
-    return apiCall({
-        url: 'v1/customer/search',
-        params: {
-            q: customerName
-        }
-    });
+  return apiCall({
+    url: 'v1/customer/search',
+    params: {
+      q: customerName
+    }
+  });
 };
 
 export const addInvoice = invoice => {
-    return apiCall({
-        url: 'v1/invoice',
-        method: 'POST',
-        data: invoice
-    });
+  return apiCall({
+    url: 'v1/invoice',
+    method: 'POST',
+    data: invoice
+  });
 };
 
 export const getInvoices = () => {
-    return apiCall({
-        url: 'v1/invoice'
-    });
+  return apiCall({
+    url: 'v1/invoice'
+  });
 };
 
 export const addProduct = product => {
-    return apiCall({
-        url: 'v1/product',
-        method: 'POST',
-        data: product
-    });
+  return apiCall({
+    url: 'v1/product',
+    method: 'POST',
+    data: product
+  });
 };
 
 export const getProducts = () => {
-    return apiCall({
-        url: 'v1/product'
-    });
+  return apiCall({
+    url: 'v1/product'
+  });
 };
 
 export const getProvinces = () => {
-    return apiCall({
-        url: 'v1/province'
-    });
+  return apiCall({
+    url: 'v1/province'
+  });
 };
 
 export const getNewInvoiceNo = () => {
-    return apiCall({
-        url: 'v1/invoice/new-invoice-no'
-    });
+  return apiCall({
+    url: 'v1/invoice/new-invoice-no'
+  });
 };
