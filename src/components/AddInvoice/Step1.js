da6a3143b8dc4fe45f72b2375437dd1276b939bb
pezhmanparsaee <p.parsaee@gmail.com>
2020-05-10 17:55:32 +0430

    fix code linting problems in Step1.js

--- src/components/AddInvoice/Step1.js
+++ src/components/AddInvoice/Step1.js
@@ -20,164 +20,164 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import appStyle from '../../styles/jss/layouts/appStyle';
 
 class Step1 extends PureComponent {
-    render() {
-        const { classes } = this.props;
-        return (
-            <div className="container">
-                <GridContainer>
-                    <GridItem xs={12} sm={12} md={6}
-                    style={{
-                        marginBottom: "30px"
-                    }}
-                    >
-                    <Typography 
-                        style={{
-                        fontWeight: "bold"
-                        }}
-                    >
-                        استان
-                    </Typography>
-                    <Select
-                        value={this.props.provinceId}
-                        onChange={this.props.onProvinceChange}
-                        className={classes.selectBox}
-                    >
-                        {this.props.provinces.map(province => {
-                        return (
-                            <MenuItem key={province._id} value={province._id}>
-                            { province.name }
-                            </MenuItem>
-                        );
-                        })}
-                    </Select>
-                    </GridItem>
-                    <GridItem xs={12} sm={12} md={6}
-                    style={{
-                        marginBottom: "30px"
-                    }}
-                    >
-                    <Typography
-                        style={{
-                        fontWeight: "bold"
-                        }}
-                    >
-                        شهر
-                    </Typography>
-                    <Select
-                        value={this.props.cityId}
-                        onChange={this.props.onCityChange}
-                        className={classes.selectBox}
-                    >
-                        {this.props.provinces
-                        .find(province => {
-                            return province._id === this.props.provinceId;
-                        }) ?
-                        this.props.provinces
-                            .find(province => {
-                            return province._id === this.props.provinceId;
-                            })
-                            .cities
-                            .map(cityInProvince => {
-                            return (
-                                <MenuItem key={cityInProvince._id} value={cityInProvince._id}>
-                                { cityInProvince.name }
-                                </MenuItem>
-                            );
-                            }): null
-                        }
-                    </Select>
-                    </GridItem>
-                    <GridItem xs={12} sm={12} md={12}
-                    style={{
-                        marginBottom: "30px"
-                    }}
-                    >
-                    <FormControl component="fieldset" className="formControl">
-                        <FormLabel component="legend"
-                        style={{
-                            fontWeight: "bold"
-                        }}
-                        >نوع پست</FormLabel>
-                        <RadioGroup
-                            aria-label="MailType"
-                            name="mailType"
-                            className={classes.group}
-                            value={this.props.mailType}
-                            onChange={this.props.onMailTypeChange}
-                            style={{display: 'flex', flexDirection: 'row'}}
-                        >
-                        <FormControlLabel value="registered" control={<Radio />} label="عادی" />
-                        <FormControlLabel value="certified" control={<Radio />} label="پیشتاز" />
-                        </RadioGroup>
-                    </FormControl>
-                    </GridItem>
-                    <GridItem xs={12} sm={12} md={12}
-                    style={{
-                        marginBottom: "30px"
-                    }}
-                    >
-                        <FormControl className="formControl" >
-                            <FormLabel component="legend"
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className="container">
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={6}
+            style={{
+              marginBottom: "30px"
+            }}
+          >
+            <Typography 
+              style={{
+                fontWeight: "bold"
+              }}
+            >
+                استان
+            </Typography>
+            <Select
+              value={this.props.provinceId}
+              onChange={this.props.onProvinceChange}
+              className={classes.selectBox}
+            >
+              {this.props.provinces.map(province => {
+                return (
+                  <MenuItem key={province._id} value={province._id}>
+                    { province.name }
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </GridItem>
+          <GridItem xs={12} sm={12} md={6}
+            style={{
+              marginBottom: "30px"
+            }}
+          >
+            <Typography
+              style={{
+                fontWeight: "bold"
+              }}
+            >
+                شهر
+            </Typography>
+            <Select
+              value={this.props.cityId}
+              onChange={this.props.onCityChange}
+              className={classes.selectBox}
+            >
+              {this.props.provinces
+                .find(province => {
+                  return province._id === this.props.provinceId;
+                }) ?
+                this.props.provinces
+                  .find(province => {
+                    return province._id === this.props.provinceId;
+                  })
+                  .cities
+                  .map(cityInProvince => {
+                    return (
+                      <MenuItem key={cityInProvince._id} value={cityInProvince._id}>
+                        { cityInProvince.name }
+                      </MenuItem>
+                    );
+                  }): null
+              }
+            </Select>
+          </GridItem>
+          <GridItem xs={12} sm={12} md={12}
+            style={{
+              marginBottom: "30px"
+            }}
+          >
+            <FormControl component="fieldset" className="formControl">
+              <FormLabel component="legend"
+                style={{
+                  fontWeight: "bold"
+                }}
+              >نوع پست</FormLabel>
+              <RadioGroup
+                aria-label="MailType"
+                name="mailType"
+                className={classes.group}
+                value={this.props.mailType}
+                onChange={this.props.onMailTypeChange}
+                style={{display: 'flex', flexDirection: 'row'}}
+              >
+                <FormControlLabel value="registered" control={<Radio />} label="عادی" />
+                <FormControlLabel value="certified" control={<Radio />} label="پیشتاز" />
+              </RadioGroup>
+            </FormControl>
+          </GridItem>
+          <GridItem xs={12} sm={12} md={12}
+            style={{
+              marginBottom: "30px"
+            }}
+          >
+            <FormControl className="formControl" >
+              <FormLabel component="legend"
                             
-                            >تاریخ تحویل</FormLabel>                
+              >تاریخ تحویل</FormLabel>                
                             
-                            <div style={{display: 'flex', flexDirection: 'row'}}>
-                                <TextField
-                                    type="number"
-                                    value={this.props.deliverAfter}
-                                    onChange={this.props.onDeliverAfterChange}
-                                    style={{
-                                        display: "inline-block",
-                                        marginLeft: "10px"
-                                    }}
-                                />
+              <div style={{display: 'flex', flexDirection: 'row'}}>
+                <TextField
+                  type="number"
+                  value={this.props.deliverAfter}
+                  onChange={this.props.onDeliverAfterChange}
+                  style={{
+                    display: "inline-block",
+                    marginLeft: "10px"
+                  }}
+                />
 
-                                <Select
-                                    value={this.props.deliverAfterTimeUnit}
-                                    onChange={this.props.onDeliverAfterTimeUnitChange}
-                                    className={classes.selectBox}
-                                    style={{
-                                        display: "inline-block",
-                                        marginLeft: "10px"
-                                    }}
-                                >
-                                    <MenuItem key="hour" value="hour">ساعت</MenuItem>
-                                    <MenuItem key="day" value="day">روز</MenuItem>
-                                    <MenuItem key="month" value="month">ماه</MenuItem>
-                                </Select>
+                <Select
+                  value={this.props.deliverAfterTimeUnit}
+                  onChange={this.props.onDeliverAfterTimeUnitChange}
+                  className={classes.selectBox}
+                  style={{
+                    display: "inline-block",
+                    marginLeft: "10px"
+                  }}
+                >
+                  <MenuItem key="hour" value="hour">ساعت</MenuItem>
+                  <MenuItem key="day" value="day">روز</MenuItem>
+                  <MenuItem key="month" value="month">ماه</MenuItem>
+                </Select>
 
-                                <Typography
-                                    style={{
-                                        display: "inline-block",
-                                        marginLeft: "10px"
-                                    }}
-                                >
+                <Typography
+                  style={{
+                    display: "inline-block",
+                    marginLeft: "10px"
+                  }}
+                >
                                     بعد از تاریخ ثبت سفارش
-                                </Typography>
+                </Typography>
 
-                            </div>
+              </div>
                             
-                        </FormControl>
-                    </GridItem>
-                </GridContainer>
-            </div>
-        )
-    }
+            </FormControl>
+          </GridItem>
+        </GridContainer>
+      </div>
+    )
+  }
 }
 
 Step1.propTypes = {
-    classes: PropTypes.object.isRequired,
-    provinceId: PropTypes.string.isRequired,
-    cityId: PropTypes.string.isRequired,
-    onProvinceChange: PropTypes.func.isRequired,
-    onCityChange: PropTypes.func.isRequired,
-    provinces: PropTypes.array.isRequired,
-    mailType: PropTypes.string.isRequired,
-    onMailTypeChange: PropTypes.func.isRequired,
-    deliverAfter: PropTypes.string.isRequired,
-    onDeliverAfterChange: PropTypes.func.isRequired,
-    deliverAfterTimeUnit: PropTypes.string.isRequired,
-    onDeliverAfterTimeUnitChange: PropTypes.func.isRequired
+  classes: PropTypes.object.isRequired,
+  provinceId: PropTypes.string.isRequired,
+  cityId: PropTypes.string.isRequired,
+  onProvinceChange: PropTypes.func.isRequired,
+  onCityChange: PropTypes.func.isRequired,
+  provinces: PropTypes.array.isRequired,
+  mailType: PropTypes.string.isRequired,
+  onMailTypeChange: PropTypes.func.isRequired,
+  deliverAfter: PropTypes.string.isRequired,
+  onDeliverAfterChange: PropTypes.func.isRequired,
+  deliverAfterTimeUnit: PropTypes.string.isRequired,
+  onDeliverAfterTimeUnitChange: PropTypes.func.isRequired
 };
 
 export default withStyles(appStyle)(Step1);
