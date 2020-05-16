5f7b8b7f40a1bda022d7149a87fb6e81bcc2962d
pezhmanparsaee <p.parsaee@gmail.com>
2020-05-17 00:08:30 +0430

    move InvoiceProductsList codes to a separate component

---
+++ src/components/AddInvoice/InvoiceProductsList.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import InvoiceProductsListItem from './InvoiceProductsListItem';
+import {List, ListItem, Select, MenuItem, TextField, Typography } from '@material-ui/core';
+
+// @material-ui/icons
+import Icon from '@material-ui/core/Icon';
+import IconButton from '@material-ui/core/IconButton';
+
+import {separateDigits} from '../../helpers/numberHelpers';
+
+export const InvoiceProductsList = props => {
+  const onAddNewProductToInvoice = () => {
+
+  };
+
+  return (
+    <List>
+      <ListItem className={props.classes.addFactorProductsListItemHead} key={`iplh_1`}>
+        <List className={props.classes.addFactorProductsNestedList}>
+          <ListItem style={{width:'30%'}}
+            className={props.classes.addFactorProductsNestedListItemHead}
+            key={`iplh_1_1`}
+          >نام محصول</ListItem>
+          <ListItem style={{width:'20%'}}
+            className={props.classes.addFactorProductsNestedListItemHead}
+            key={`iplh_1_2`}
+          >تعداد</ListItem>
+          <ListItem style={{width:'20%'}}
+            className={props.classes.addFactorProductsNestedListItemHead}
+            key={`iplh_1_3`}>قیمت واحد</ListItem>
+          <ListItem style={{width:'20%'}}
+            className={props.classes.addFactorProductsNestedListItemHead}
+            key={`iplh_1_4`}>قیمت کل</ListItem>
+          <ListItem style={{width:'10%'}}
+            key={`iplh_1_5`}></ListItem>
+        </List>
+      </ListItem>
+      
+      {
+        props.products.map(product => {
+          return <InvoiceProductsListItem 
+            key={`ipli1_${product.id}`}
+            itemKey={`ipli_${product.id}_i`}
+            onAddedProductCountChange={props.onAddedProductCountChange}
+            product={{
+              id: product.id,
+              name: product.name,
+              unitPrice: product.unitPrice,                              
+              count: product.count,
+              totalPrice: product.totalPrice
+            }}
+          />
+        })
+      }
+      
+      <ListItem className={props.classes.addFactorProductsListItem} key={`ipli_add`}>
+        <List className={props.classes.addFactorProductsNestedList}>
+          <ListItem style={{width:'30%'}}
+            className={props.classes.addFactorProductsNestedListItem}
+            key={`ipli_add_1`}
+          >
+            <IconButton onClick={props.onOpenAddProductDialog}>
+              <Icon>add_circle</Icon>
+            </IconButton>
+            <Select
+              value={props.newProductId}
+              onChange={props.onNewProductSelectChange}
+              className={props.classes.selectBox}
+            >
+              {props.products.filter(product => {
+                const found = props.products.find(invoiceProduct => {
+                  return invoiceProduct.productId === product._id;
+                });
+                return !found;
+              }).map(product => {
+                return (
+                  <MenuItem key={product._id} value={product._id}>
+                    {product.name}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </ListItem>
+          <ListItem style={{width:'20%'}}
+            className={props.classes.addFactorProductsNestedListItem}
+            key={`ipli_add_2`}
+          >
+            <TextField
+              type="number"
+              value={props.newProductCount}
+              onChange={props.onNewProductCountChange}
+              className="text-center"
+            >
+            </TextField>
+          </ListItem>
+          <ListItem style={{width:'20%'}}
+            className={props.classes.addFactorProductsNestedListItem}
+            key={`ipli_add_3`}
+          >
+            <Typography>{separateDigits({number: props.newProductUnitPrice, showCurrency: true})}</Typography>
+          </ListItem>
+          <ListItem style={{width:'20%'}}
+            className={props.classes.addFactorProductsNestedListItem}
+            key={`ipli_add_4`}
+          >
+            <Typography>{separateDigits({number: props.newProductTotalPrice})}</Typography>
+          </ListItem>
+          <ListItem style={{width:'10%'}}
+            className={props.classes.addFactorProductsNestedListItem}
+            key={`ipli_add_5`}
+          >
+            <IconButton 
+              onClick={onAddNewProductToInvoice}
+            >
+              <Icon>add_circle</Icon>
+            </IconButton>
+          </ListItem>
+        </List>
+      </ListItem>
+    </List>
+  );
+};
--- src/components/AddInvoice/Step0.js
+++ src/components/AddInvoice/Step0.js
@@ -6,26 +6,20 @@ import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import FormControl from '@material-ui/core/FormControl';
 import { withStyles } from '@material-ui/core';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Select from '@material-ui/core/Select';
-import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
-import MenuItem from '@material-ui/core/MenuItem';
 
-// @material-ui/icons
-import Icon from '@material-ui/core/Icon';
-import IconButton from '@material-ui/core/IconButton';
+
+// // @material-ui/icons
+
 
 // local dependencies
 import appStyle from '../../styles/jss/layouts/appStyle';
-import { separateDigits } from '../../helpers/numberHelpers';
 
 // local components
 import AddProductDialog from '../AddProductDialog';
 import GridContainer from '../Grid/GridContainer';
 import GridItem from '../Grid/GridItem';
-import InvoiceProductsListItem from './InvoiceProductsListItem';
+// import InvoiceProductsListItem from './InvoiceProductsListItem';
 
 class Step0 extends PureComponent {
   render() {
@@ -79,110 +73,7 @@ class Step0 extends PureComponent {
         </GridContainer>
         <GridContainer>
           <GridItem xs={12} sm={12} md={12}>
-            <List>
-              <ListItem className={ classes.addFactorProductsListItemHead } key={ `iplh_1` }>
-                <List className={ classes.addFactorProductsNestedList }>
-                  <ListItem style={{width:'30%'}}
-                    className={ classes.addFactorProductsNestedListItemHead }
-                    key={ `iplh_1_1` }
-                  >نام محصول</ListItem>
-                  <ListItem style={{width:'20%'}}
-                    className={ classes.addFactorProductsNestedListItemHead }
-                    key={ `iplh_1_2` }
-                  >تعداد</ListItem>
-                  <ListItem style={{width:'20%'}}
-                    className={ classes.addFactorProductsNestedListItemHead }
-                    key={ `iplh_1_3` }>قیمت واحد</ListItem>
-                  <ListItem style={{width:'20%'}}
-                    className={ classes.addFactorProductsNestedListItemHead }
-                    key={ `iplh_1_4` }>قیمت کل</ListItem>
-                  <ListItem style={{width:'10%'}}
-                    key={ `iplh_1_5` }></ListItem>
-                </List>
-              </ListItem>
-              
-              { 
-                this.props.invoiceProducts.map((product, index) => {
-                  return <InvoiceProductsListItem 
-                    key={ `ipli1_${index}` }
-                    itemKey={ `ipli_${index}_i` }
-                    onAddedProductCountChange={ this.props.onAddedProductCountChange }
-                    product={{
-                      id: product.id,
-                      name: product.name,
-                      unitPrice: product.unitPrice,                              
-                      count: product.count,
-                      totalPrice: product.totalPrice
-                    }}
-                  />
-                })
-              }
-              
-              <ListItem className={ classes.addFactorProductsListItem } key={ `ipli_add` }>
-                <List className={ classes.addFactorProductsNestedList }>
-                  <ListItem style={{width:'30%'}}
-                    className={ classes.addFactorProductsNestedListItem }
-                    key={ `ipli_add_1` }
-                  >
-                    <IconButton onClick={this.props.onOpenAddProductDialog}>
-                      <Icon>add_circle</Icon>
-                    </IconButton>
-                    <Select
-                      value={this.props.newProductId}
-                      onChange={this.props.onNewProductSelectChange}
-                      className={classes.selectBox}
-                    >
-                      {this.props.products.filter(product => {
-                        const found = this.props.invoiceProducts.find(invoiceProduct => {
-                          return invoiceProduct.productId === product._id;
-                        });
-                        return !found;
-                      }).map(product => {
-                        return (
-                          <MenuItem key={product._id} value={product._id}>
-                            { product.name }
-                          </MenuItem>
-                        );
-                      })}
-                    </Select>
-                  </ListItem>
-                  <ListItem style={{width:'20%'}}
-                    className={ classes.addFactorProductsNestedListItem }
-                    key={ `ipli_add_2` }
-                  >
-                    <TextField
-                      type="number"
-                      value={this.props.newProductCount}
-                      onChange={this.props.onNewProductCountChange}
-                      className="text-center"
-                    >
-                    </TextField>
-                  </ListItem>
-                  <ListItem style={{width:'20%'}}
-                    className={ classes.addFactorProductsNestedListItem }
-                    key={ `ipli_add_3` }
-                  >
-                    <Typography>{ separateDigits({ number: this.props.newProductUnitPrice, showCurrency: true }) }</Typography>
-                  </ListItem>
-                  <ListItem style={{width:'20%'}}
-                    className={ classes.addFactorProductsNestedListItem }
-                    key={ `ipli_add_4` }
-                  >
-                    <Typography>{ separateDigits({ number: this.props.newProductTotalPrice }) }</Typography>
-                  </ListItem>
-                  <ListItem style={{width:'10%'}}
-                    className={ classes.addFactorProductsNestedListItem }
-                    key={ `ipli_add_5` }
-                  >
-                    <IconButton 
-                      onClick={this.onAddNewProductToInvoice}
-                    >
-                      <Icon>add_circle</Icon>
-                    </IconButton>
-                  </ListItem>
-                </List>
-              </ListItem>
-            </List>
+            
           </GridItem>
         </GridContainer>
         <GridContainer>
