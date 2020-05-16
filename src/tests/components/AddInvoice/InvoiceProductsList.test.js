60a64bbfa5cceaec41832b3486a6b33f6c3f68fc
pezhmanparsaee <p.parsaee@gmail.com>
2020-05-17 00:08:56 +0430

    add a render test for InvoiceProductsList component

---
+++ src/tests/components/AddInvoice/InvoiceProductsList.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { InvoiceProductsList } from '../../../components/AddInvoice/InvoiceProductsList';
+import products from '../../fixtures/products';
+
+test('should render InvoiceProductsList with products', () => {
+  const wrapper = shallow(<InvoiceProductsList products={products} classes={{}} />);
+  expect(wrapper).toMatchSnapshot();
+});
