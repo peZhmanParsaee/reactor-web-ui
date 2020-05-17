eed1e96380dd2f36d060792f3af245c4a6f8d0e2
pezhmanparsaee <p.parsaee@gmail.com>
2020-05-17 08:11:04 +0430

    write some test cases for provinces async action creators

--- src/tests/actions/products.test.js
+++ src/tests/actions/products.test.js
@@ -1,19 +1,19 @@
 import * as productsActions from '../../actions/products';
 import * as types  from '../../actions/actionTypes';
 
-test('should set up add product action object', () => {
+test('should setup add product action object with provided values', () => {
   const action = productsActions.addProduct({ name: 'prod 1', unitPrice: 1200, count: 300 });
   expect(action).toEqual({
     type: types.ADD_PRODUCT,
     payload : {
       name: 'prod 1', 
       unitPrice: 1200, 
-      count: 300      
+      count: 300
     }
   });
 });
 
-test('should set up set products action object', () => {
+test('should setup set products action object with empty array', () => {
   const action = productsActions.setProducts([]);
   expect(action).toEqual({
     type: types.SET_PRODUCTS,
--- src/tests/actions/provinces.test.js
+++ src/tests/actions/provinces.test.js
@@ -1,5 +1,15 @@
+import configureStore from 'redux-mock-store';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import thunk from 'redux-thunk';
+
 import * as provincesActions from '../../actions/provinces';
 import * as types from '../../actions/actionTypes';
+import provinces from '../fixtures/provinces';
+
+global.API_ENDPOINT = 'http://localhost:1234';
+
+const getUrl = route => `${API_ENDPOINT}/api/${route}`;
 
 describe('provinces actions', () => {
   it('should return set provinces type correctly', () => {
@@ -7,15 +17,45 @@ describe('provinces actions', () => {
     expect(action.type).toBe(types.SET_PROVINCES);
   });
   it('should return set provinces action object correctly', () => {
-    const provinces = [
-      'California',
-      'Texas',
-      'Florida'
-    ];
     const action = provincesActions.setProvinces(provinces);
     expect(action).toEqual({
       type: types.SET_PROVINCES,
       payload: provinces
     });
   })
+});
+
+describe('provinces async actions', () => {  
+  const middlewares = [thunk];
+  const mockStore = configureStore(middlewares);  
+  const mock = new MockAdapter(axios);
+
+  it('should create SET_PROVINCES correctly', () => {
+    const initialState = {};
+    const store = mockStore(initialState);
+    store.dispatch(provincesActions.setProvinces(provinces));
+    const actions = store.getActions();
+    const expectedActions = [{ type: types.SET_PROVINCES, payload: provinces }];
+    expect(actions).toEqual(expectedActions);
+  });
+
+  it('should create SET_PROVINCES when fetching provinces has been done', () => {
+    mock.onGet(getUrl('v1/province'))
+      .reply(200, {
+        status: true,
+        payload: provinces        
+      });
+    
+    const initialState = { provinces: [] };
+    const store = mockStore(initialState);
+
+    store.dispatch(provincesActions.startSetProvinces())
+      .then(() => {
+        const expectedActions = [{
+          type: types.SET_PROVINCES,
+          payload: provinces
+        }];
+        expect(store.getActions()).toEqual(expectedActions)
+      });
+  });
 });
