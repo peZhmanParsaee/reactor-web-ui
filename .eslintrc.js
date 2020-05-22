ca7d079c5f63b0b7a3c67e63bd089d228c586999
pezhmanparsaee <p.parsaee@gmail.com>
2020-05-22 16:17:33 +0430

    change comma-dangle and no-console rules of eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,40 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "jest": true
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  extends: 'airbnb',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    'jest/globals': true,
+    API_ENDPOINT: true
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": "airbnb",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly",
-        "jest/globals": true,
-        "API_ENDPOINT": true
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "jest"
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'jest'],
+  rules: {
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'local',
+        args: 'none'
+      }
     ],
-    "rules": {
-        "no-unused-vars": [
-            "error",
-            {
-                "vars": "local",
-                "args": "none"
-            }
-        ],
-        "comma-dangle": "off",
-        "react/jsx-filename-extension": 0,
-        "jest/no-disabled-tests": "warn",
-        "jest/no-focused-tests": "error",
-        "jest/no-identical-title": "error",
-        "jest/prefer-to-have-length": "warn",
-        "jest/valid-expect": "error",
-        "indent": [
-            "error",
-            2
-        ],
-    }
+    'comma-dangle': 'off',
+    'react/jsx-filename-extension': 0,
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+    indent: ['error', 2],
+    'no-console': 'off'
+  }
 };
