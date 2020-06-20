6dddcbb0c9afb4e7b8aa8f5bd1d60a35793b3115
pezhmanparsaee <p.parsaee@gmail.com>
2020-06-20 16:30:05 +0430

    export/import named redux middewares

---
+++ src/redux/middlewares/index.js
@@ -0,0 +1,5 @@
+import api from './api';
+import logger from './logger';
+
+export const apiMiddleware = api;
+export const loggerMiddleware = logger;
--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
-import apiMiddleware from '../middlewares/api';
-import loggerMiddleware from '../middlewares/logger';
+import { loggerMiddleware, apiMiddleware } from '../middlewares';
 import reducers from '../reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
