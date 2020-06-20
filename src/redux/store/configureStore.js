02fe65b893e3d7f218cb533eadff829140922c5c
pezhmanparsaee <p.parsaee@gmail.com>
2020-06-20 15:07:31 +0430

    add logger middleware to redux store

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -1,15 +1,16 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
-// import apiMiddleware from '../middlewares/api';
+import apiMiddleware from '../middlewares/api';
+import loggerMiddleware from '../middlewares/logger';
 import reducers from '../reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => {
-  const store = createStore(
-    reducers,
-    // applyMiddleware(apiMiddleware)
-    composeEnhancers(applyMiddleware(reduxThunk))
-  );
+  const middlewares = [loggerMiddleware, reduxThunk, apiMiddleware];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
+  const composedEnhancers = composeEnhancers(middlewareEnhancer);
+
+  const store = createStore(reducers, composedEnhancers);
   return store;
 };
