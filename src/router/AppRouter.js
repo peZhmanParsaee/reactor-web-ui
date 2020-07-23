b480953f44aee6323ee0154bd799b97a74e5ea13
pezhmanparsaee <p.parsaee@gmail.com>
2020-07-23 15:08:18 +0430

    remove using of material components in AppRouter

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
-import { MuiThemeProvider } from '@material-ui/core/styles';
 import { createBrowserHistory } from 'history';
 import NotFoundPage from '../screens/shared/NotFoundPage';
-import AppTheme from '../themes/AppTheme';
 
 import Layout from '../screens/shared/Layout';
 
 const history = createBrowserHistory();
 
 const AppRouter = () => (
-  <MuiThemeProvider theme={AppTheme}>
-    <Router history={history}>
-      <div>
-        <Switch>
-          <Route path="/" component={Layout} />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </div>
-    </Router>
-  </MuiThemeProvider>
+  <Router history={history}>
+    <div>
+      <Switch>
+        <Route path="/" component={Layout} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </Router>
 );
 
 export default AppRouter;
