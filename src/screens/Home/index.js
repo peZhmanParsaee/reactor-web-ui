239a7b902da38bf01c6bfb59493a02f6c3d59134
pezhmanparsaee <p.parsaee@gmail.com>
2020-07-23 15:07:59 +0430

    change home page

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,64 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-
-// @material-ui/core
-import { withStyles } from '@material-ui/core';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import DescriptionIcon from '@material-ui/icons/Description';
-import ReceiptIcon from '@material-ui/icons/Receipt';
-import CssBaseline from '@material-ui/core/CssBaseline';
 
 // local dependencies
 import Header from '../../components/Header';
 import homePageStyle from '../../styles/jss/components/homePageStyle';
 
-class HomePage extends React.PureComponent {
-  componentWillMount() {
-    console.log('HomePage componentWillMount');
-  }
-
-  componentWillUnmount() {
-    console.log('HomePage componentWillUnmount');
-  }
-
+class HomePage extends Component {
   render() {
-    const { classes } = this.props;
-
     return (
-      <>
-        <CssBaseline />
-        <Header />
-        <List>
-          <ListItem button component={Link} to="/add-invoice">
-            <ListItemIcon>
-              <ReceiptIcon />
-            </ListItemIcon>
-            <ListItemText
-              primary="ثبت فاکتور جدید"
-              className={classes.mainNavItem}
-            />
-          </ListItem>
-          <ListItem button component={Link} to="/sales-report">
-            <ListItemIcon>
-              <DescriptionIcon />
-            </ListItemIcon>
-            <ListItemText
-              primary="گزارش فروش"
-              className={classes.mainNavItem}
-            />
-          </ListItem>
-        </List>
-      </>
+      <div className="wrapper">
+        <header>header</header>
+        <nav>
+          <ul>
+            <li>Home Page</li>
+            <li>Search</li>
+            <li>About us</li>
+          </ul>
+        </nav>
+        <main>Main Content</main>
+        <footer>Footer</footer>
+      </div>
     );
   }
 }
 
-HomePage.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(homePageStyle)(HomePage);
+export default HomePage;
--- src/screens/shared/Layout.js
+++ src/screens/shared/Layout.js
@@ -2,49 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
-// @material-ui/core
-import { withStyles } from '@material-ui/core';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-
 // local components
+import HomePage from '../Home';
 import AddInvoicePage from '../Invoice/Add';
 import SalesReportPage from '../Invoice/List';
 import Footer from '../../components/Footer';
 import ToastMessage from '../../components/ToastMessage';
-
-// local dependencies
-import layoutStyle from '../../styles/jss/layouts/layoutStyle';
-import AppTheme from '../../themes/AppTheme';
 
 class Layout extends React.Component {
   areWeInReportRoute = () => {
     return this.props.location.pathname === '/sales-report';
   };
   render() {
-    const { classes } = this.props;
-
     return (
-      <MuiThemeProvider theme={AppTheme}>
-        <CssBaseline />
-        <div className={classes.wrapper}>
-          <Switch>
-            <Route path="/add-invoice" component={AddInvoicePage} />
-            <Route path="/sales-report" component={SalesReportPage} />
-            <Redirect from="/" to="/add-invoice" />
-          </Switch>
+      <div>
+        <Switch>
+          <Route path="/" component={HomePage} />
+          <Route path="/add-invoice" component={AddInvoicePage} />
+          <Route path="/sales-report" component={SalesReportPage} />
+        </Switch>
 
-          <ToastMessage />
+        {/* <ToastMessage /> */}
 
-          {this.areWeInReportRoute() ? null : <Footer />}
-        </div>
-      </MuiThemeProvider>
+        {/* {this.areWeInReportRoute() ? null : <Footer />} */}
+      </div>
     );
   }
 }
 
-Layout.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(layoutStyle)(Layout);
+export default Layout;
