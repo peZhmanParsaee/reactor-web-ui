d95c775ab3bb13147f7dedb7f96d831a05f12f72
pezhmanparsaee <p.parsaee@gmail.com>
2020-08-02 23:42:10 +0430

    change webpack config file (loaders sections)

--- webpack.config.js
+++ webpack.config.js
@@ -6,32 +6,48 @@ module.exports = (env) => {
   const isProduction = env === 'production';
 
   return {
-    "mode": "development",
-    entry: ["babel-polyfill", './src/app.js'],
+    mode: 'development',
+    entry: ['babel-polyfill', './src/app.js'],
     output: {
       path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
     module: {
-      rules: [{
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      }, {
-        test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }, 
-    ]
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          loader: 'file-loader',
+          options: {
+            publicPath: 'dist',
+            // outputPath: 'images',
+            name: 'assets/images/[name]_[md5:hash:base64:10].[ext]'
+          }
+        },
+        {
+          test: /\.(woff|woff2|ttf|otf|eot)$/,
+          loader: 'file-loader',
+          options: {
+            publicPath: 'dist',
+            name: 'assets/fonts/[name]_[md5:hash:base64:10].[ext]'
+          }
+        }
+      ]
     },
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
-      publicPath: '/dist/'
+      publicPath: '/dist/',
+      open: false
     },
     plugins: [
       new webpack.DefinePlugin({
@@ -39,5 +55,4 @@ module.exports = (env) => {
       })
     ]
   };
-
-}
+};
